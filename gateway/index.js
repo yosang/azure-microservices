617c6992faa2e90de2c68c4077c4a2b0ae309052
Yosmel Chiang <yosmelchiang@gmail.com>
2025-09-30 11:47:00 +0200

    add an entry for home in the gateway

--- gateway/index.js
+++ gateway/index.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 app.use("/cities", proxy("http://localhost:3000"));
 app.use("/colors", proxy("http://localhost:3001"));
+app.use("/", proxy("http://localhost:3002"));
 
 app.listen(8000, (err) => {
   if (err) return console.log("Gateway failed to run");
